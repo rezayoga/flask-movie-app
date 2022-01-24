b9dc9f954343d585a0f1e61d2af277c1e7e97a86
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-24 07:31:59 +0700

    Updating

---
+++ app/src/components/Movie.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Movie",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {
+    getUsers() {
+      const path = "https://movieapi.rezayogaswara.com/auth/signin";
+      axios({
+            method: 'get',
+            url: path,
+            auth: {
+              username: 'reza',
+              password: 'reza'
+            }
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getUsers();
+  },
+};
+</script>
